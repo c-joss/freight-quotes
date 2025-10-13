9dec5f2e16f53b4661e8bb0226af93577365d3d1
Courtney Macgregor <cjoss90@hotmail.com>
2025-10-13 23:58:20 +1000

    feat(seed): add port pairs and flush to get lane IDs

--- server/seed.py
+++ server/seed.py
@@ -28,3 +28,8 @@ if __name__ == '__main__':
         tyo = Port(name="Tokyo", code="JPTYO")
         db.session.add_all([mel, syd, tyo])
         db.session.flush()
+
+        pp1 = PortPair(origin_port_id=mel.id, destination_port_id=tyo.id)
+        pp2 = PortPair(origin_port_id=syd.id, destination_port_id=tyo.id)
+        db.session.add_all([pp1, pp2])
+        db.session.flush()
