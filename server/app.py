72d22729e1a2ea4d05f1e446a7e2d199c7755edf
Courtney Macgregor <cjoss90@hotmail.com>
2025-10-13 14:04:38 +1000

    chore(api): import flask tools, api/db, and models

--- server/app.py
+++ server/app.py
@@ -3,12 +3,13 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
+from .models import User, Port, PortPair, ContainerType, Rate, Quote, QuoteRate
 
 
 # Views go here!
