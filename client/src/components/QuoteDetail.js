8dfee0817f3566afb5d18546a9ba447aeb17b9e3
Courtney Macgregor <cjoss90@hotmail.com>
2025-10-14 22:58:02 +1000

    chore(quote-detail): add imports for hooks and formik

--- client/src/components/QuoteDetail.js
+++ client/src/components/QuoteDetail.js
@@ -1,3 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+
 import { useParams } from 'react-router-dom';
 
 export default function QuoteDetail({ user }) {
