a93555fb000a09b2cfe2c7cd1404985968ff7f69
Courtney Macgregor <cjoss90@hotmail.com>
2025-10-14 21:55:36 +1000

    feat(new-quote): add title and select fields with validation

--- client/src/components/NewQuote.js
+++ client/src/components/NewQuote.js
@@ -39,7 +39,47 @@ export default function NewQuote({ user }) {
                     validationSchema={schema}
                     onSubmit={() => {}}
                 >
-                    <Form>tba</Form>                    
+                    <Form style={{ display: "grid", gap: 8, maxWidth: 480 }}>
+                        <label>Title</label>
+                        <Field name="title" />
+                        <ErrorMessage name="title" component="div" style={{ color: "red" }} />
+
+                        <label>Port Pair</label>
+                        <Field
+                            as="select"
+                            name="port_pair_id"
+                            onChange={(e) => {
+                                const val = Number(e.target.value);
+                                setFieldValue("port_pair_id", val);
+                                loadRates(val, values.container_type_id);
+                            }}
+                        >
+                            <option value="">Choose…</option>
+                            {pairs.map((pp) => (
+                                <option key={pp.id} value={pp.id}>
+                                    {pp.origin_port.name} → {pp.destination_port.name}
+                                </option>
+                            ))}
+                        </Field>
+                        <ErrorMessage name="port_pair_id" component="div" style={{ color: "red" }} />
+
+                        <label>Container Type</label>
+                        <Field
+                            as="select"
+                            name="container_type_id"
+                            onChange={(e) => {
+                                const val = Number(e.target.value);
+                                setFieldValue("container_type_id", val);
+                                loadRates(values.port_pair_id, val);
+                            }}
+                        >
+                            <option value="">Choose…</option>
+                            {types.map((ct) => (
+                                <option key={ct.id} value={ct.id}>{ct.code}</option>
+                            ))}
+                        </Field>
+                        <ErrorMessage name="container_type_id" component="div" style={{ color: "red" }} />
+                    </Form>                   
                 </Formik>                
             )}
         </div>
