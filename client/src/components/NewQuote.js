2b1bad7e5eef0a362e275000d7b5239fffb70a30
Courtney Macgregor <cjoss90@hotmail.com>
2025-10-14 21:21:39 +1000

    chore(new-quote): add imports for router, formik, yup

--- client/src/components/NewQuote.js
+++ client/src/components/NewQuote.js
@@ -1,3 +1,8 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 export default function NewQuote({ user }) {
     if (!user) return <p>Please log in to create a quote</p>
     return (
