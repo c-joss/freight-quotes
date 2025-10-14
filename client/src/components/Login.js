cb16abd7e8f858a98d150bcb21127c0cd439a5b8
Courtney Macgregor <cjoss90@hotmail.com>
2025-10-14 11:59:31 +1000

    feat(login): add react-router, formik, yup imports & scaffold Formik with initial values

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,3 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 export default function Login() {
-  return <h2>Login</h2>;
+  const nav = useNavigate();
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={() => {}}
+      >
+        <Form>form tba</Form>
+      </Formik>
+    </div>
+  );
 }
